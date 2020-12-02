09e4d657045d15db182681bfbc8976424c3384d0
AmelieRoussel <amelie.roussel41@gmail.com>
2020-12-02 19:06:28 +0100

    quest 9 challenge

---
+++ assets/README.md
@@ -0,0 +1,3 @@
+Quête 9 :
+https://www.loom.com/share/2fdd7c89472a4c11ac7a2fa57986c98c
+
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use App\Entity\Season;
+use App\Entity\Episode;
 
 /**
  * @Route("/programs", name="program_")
@@ -42,11 +44,39 @@ class ProgramController extends AbstractController
             ->findOneBy(['id' => $id]);
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id : ' . $id . ' found in promgram\'s table.'
+                'No program found in promgram\'s table.'
             );
         }
+
+        $seasons = $program->getSeasons();
         return $this->render('program/show.html.twig', [
-            'program' => $program
+            'program' => $program,
+            'seasons' => $seasons
+        ]);
+    }
+
+    /**
+     * Getting a season by id
+     *
+     * @Route("/{program<^[0-9]+$>}/season/{season<^[0-9]+$>}", methods={"GET"}, name="season_show")
+     * @return Response
+     */
+    public function showSeason(Program $program, Season $season)
+    {
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season found in season\'s table.'
+            );
+        }
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season
         ]);
     }
 }
