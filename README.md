3bcae6cb068f7b8a44e1b72e8aafbf66b0eb2e6e
AmelieRoussel <amelie.roussel41@gmail.com>
2020-12-04 18:21:56 +0100

    quest 12 challenge

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://www.loom.com/share/e2fd9b3d47c446a6ba0dba80638b6e69
 
 Quête 11 :
 https://www.loom.com/share/ffd23b35193b4a168759988806f22d0f
+
+Quête 12 :
+https://www.loom.com/share/0fccd92f2e7e4a6aae353446038a3338
--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -2,29 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProgramType extends AbstractType
+class SeasonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('summary', TextareaType::class)
-            ->add('poster', TextType::class)
-            ->add('category', null, ['choice_label' => 'name'])
+            ->add('number', IntegerType::class)
+            ->add('year', IntegerType::class)
+            ->add('description', TextareaType::class)
+            ->add('program', null, ['choice_label' => 'title'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Program::class,
+            'data_class' => Season::class,
         ]);
     }
 }
